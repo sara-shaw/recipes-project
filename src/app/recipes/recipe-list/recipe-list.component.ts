f67385369bb7389e7301888d6b6385a36235af46
Sara <sara.667@outlook.com>
2023-06-22 20:17:05 -0600

    .

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent {}
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is a description',
+      'https://content-praleska-eu.azureedge.net/content/blog/recipes/2023/4/25/my-cinnabon-recipe/Buns-on-a-wooden-table.webp'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is a description',
+      'https://content-praleska-eu.azureedge.net/content/blog/recipes/2023/4/25/my-cinnabon-recipe/Buns-on-a-wooden-table.webp'
+    ),
+  ];
+}
